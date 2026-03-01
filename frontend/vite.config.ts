a378ea5bd0c14c54c5b88aeb71eee4b4d3de25db
Yashas Manju Urs <151382365+yashasurs@users.noreply.github.com>
2026-03-01 20:32:10 +0530

    Changed config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:8000',
         ws: true,
       },
     },
